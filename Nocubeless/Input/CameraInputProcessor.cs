a52f714ae3f9487cffc7def33f059d4ad57184cd
paulwacquet <paulwacquet@outlook.com>
2020-01-08 19:05:01 +0100

    Added MonoGame Extended, be aware of this!

--- Nocubeless/Input/CameraInputProcessor.cs
+++ Nocubeless/Input/CameraInputProcessor.cs
@@ -28,6 +28,7 @@ namespace Nocubeless
 				// would Nocubeless.Camera.Settings.ZoomPercentage be better ? moreover we can directly pass the Camera.Default to the Camera() constructor (like i did with Player)
 				// Nope, we need to keep camera settings because it's our settings pattern, CubeWorld is the only class that contains its settings because there could be many worlds later, but there are not many cameras
 				// BBMSG: So should i put PlayerSettings in Nocubeless.Settings ?
+				// SDNMSG: Camera, in Nocubeless, and Player I think rather in Player.Settings because there will be online players someday
 				Nocubeless.Camera.Zoom(Nocubeless.Settings.Camera.ZoomPercentage);
 				Nocubeless.Camera.Sensitivity = Nocubeless.Settings.Camera.SensitivityWhenZooming;
 			}
